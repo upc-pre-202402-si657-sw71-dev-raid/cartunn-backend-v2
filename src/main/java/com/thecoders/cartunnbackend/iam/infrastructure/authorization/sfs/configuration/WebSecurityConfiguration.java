55bce8e6b60201aff3f9ae82baccb1a8cc1b79df
Natanael David <106049371+NickDavid1811@users.noreply.github.com>
2024-09-23 18:28:48 -0500

    Update WebSecurityConfiguration.java

--- src/main/java/com/thecoders/cartunnbackend/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/thecoders/cartunnbackend/iam/infrastructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -21,33 +21,38 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfiguration {
+
     private final UserDetailsService userDetailsService;
+
     private final BearerTokenService tokenService;
+
     private final BCryptHashingService hashingService;
+
     private final AuthenticationEntryPoint unauthorizedRequestHandler;
 
-    public WebSecurityConfiguration(
-            @Qualifier("defaultUserDetailsService") UserDetailsService userDetailsService,
-            BearerTokenService tokenService,
-            BCryptHashingService hashingService,
-            AuthenticationEntryPoint unauthorizedRequestHandler) {
-        this.userDetailsService = userDetailsService;
-        this.tokenService = tokenService;
-        this.hashingService = hashingService;
-        this.unauthorizedRequestHandler = unauthorizedRequestHandler;
-    }
-
+    /**
+     * This method creates the Bearer Authorization Request Filter.
+     * @return The Bearer Authorization Request Filter
+     */
     @Bean
     public BearerAuthorizationRequestFilter authorizationRequestFilter() {
         return new BearerAuthorizationRequestFilter(tokenService, userDetailsService);
     }
 
+    /**
+     * This method creates the authentication manager.
+     * @param authenticationConfiguration The authentication configuration
+     * @return The authentication manager
+     */
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+    /**
+     * This method creates the authentication provider.
+     * @return The authentication provider
+     */
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         var authenticationProvider = new DaoAuthenticationProvider();
@@ -56,11 +61,22 @@ public class WebSecurityConfiguration {
         return authenticationProvider;
     }
 
+    /**
+     * This method creates the password encoder.
+     * @return The password encoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return hashingService;
     }
 
+    /**
+     * This method creates the security filter chain.
+     * It also configures the http security.
+     *
+     * @param http The http security
+     * @return The security filter chain
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(configurer -> configurer.configurationSource(_ -> {
@@ -85,5 +101,20 @@ public class WebSecurityConfiguration {
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authorizationRequestFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+
+    }
+
+    /**
+     * This is the constructor of the class.
+     * @param userDetailsService The user details service
+     * @param tokenService The token service
+     * @param hashingService The hashing service
+     * @param authenticationEntryPoint The authentication entry point
+     */
+    public WebSecurityConfiguration(@Qualifier("defaultUserDetailsService") UserDetailsService userDetailsService, BearerTokenService tokenService, BCryptHashingService hashingService, AuthenticationEntryPoint authenticationEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.tokenService = tokenService;
+        this.hashingService = hashingService;
+        this.unauthorizedRequestHandler = authenticationEntryPoint;
     }
 }

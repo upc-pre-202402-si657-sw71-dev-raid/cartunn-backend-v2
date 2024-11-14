2290a98adeac9696ff8e7b99ca9820703a3b8864
PieroM1 <105945018+PieroM1@users.noreply.github.com>
2024-11-14 16:08:53 -0500

    feat(product): added DeleteFavoriteByIdProduct function in FavoriteCommandService and FavoriteCommandServiceImpl

--- src/main/java/com/thecoders/cartunnbackend/product/application/internal/commandservices/FavoriteCommandServiceImpl.java
+++ src/main/java/com/thecoders/cartunnbackend/product/application/internal/commandservices/FavoriteCommandServiceImpl.java
@@ -3,12 +3,16 @@ package com.thecoders.cartunnbackend.product.application.internal.commandservice
 import com.thecoders.cartunnbackend.product.domain.exceptions.ProductNotFoundException;
 import com.thecoders.cartunnbackend.product.domain.model.aggregates.Favorite;
 import com.thecoders.cartunnbackend.product.domain.model.aggregates.Product;
+import com.thecoders.cartunnbackend.product.domain.model.commands.DeleteFavoriteByProductIdCommand;
 import com.thecoders.cartunnbackend.product.domain.model.commands.DeleteFavoriteCommand;
 import com.thecoders.cartunnbackend.product.domain.model.commands.RequestFavoriteCommand;
 import com.thecoders.cartunnbackend.product.domain.services.FavoriteCommandService;
 import com.thecoders.cartunnbackend.product.infrastructure.persitence.jpa.repositories.FavoriteRepository;
 import com.thecoders.cartunnbackend.product.infrastructure.persitence.jpa.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -38,6 +42,20 @@ public class FavoriteCommandServiceImpl implements FavoriteCommandService {
             favoriteRepository.deleteById(command.favoriteId());
         } catch (Exception e) {
             throw new IllegalArgumentException("Error while deleting favorite: " + e.getMessage());
+        }
+    }
+
+    @Override
+    @Transactional
+    public void handle(DeleteFavoriteByProductIdCommand command) {
+        List<Favorite> favorites = favoriteRepository.findAllByProductId(command.productId());
+        if (favorites.isEmpty()) {
+            throw new IllegalArgumentException("No favorites found for the specified product ID");
+        }
+        try {
+            favoriteRepository.deleteAll(favorites);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while deleting favorites by product ID: " + e.getMessage());
         }
     }
 
